6cc98fc0fbf1fe337febc0e161939c7b53a05b20
oreanroy <oreanroy@gmail.com>
2018-10-18 19:22:32 +0530

    added components

---
+++ src/components/Shelf.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import Book from './Book';
+
+export default class Shelf extends Component {
+    render() {
+      console.log(this.props.books);
+        return ( <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            <li>
+              {this.props.books.map((data) => <Book book={data} key={data.id}/> )}
+           </li>
+          </ol>
+        </div>
+      </div>
+       )
+    }
+}
